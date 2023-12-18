2a84ebf1bc0ac4eec220e94de18f2a4517234f8b
Ygor Leonardo Nocce <151590883+ynocce@users.noreply.github.com>
2023-12-17 23:45:44 -0300

    Delete MySQL

--- MySQL/funcoes de texto.sql
+++
@@ -1,7 +0,0 @@
-#Funções de textos
-
-SELECT UPPER(sobrenome) FROM tb_funcionario;
- 
-SELECT LOWER(sobrenome) FROM tb_funcionario;
-
-SELECT LENGTH(sobrenome) FROM tb_funcionario; 
--- MySQL/selects.sql
+++
@@ -1,9 +0,0 @@
-#Selects
-
-select nome, sobrenome, cpf from tb_funcionario;
-
-
-select * from tb_funcionario;
-
-
-select * from tb_funcionario limit 5;
